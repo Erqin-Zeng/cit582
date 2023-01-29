f25ed447f36bc7d531abb1d15929c694d0e88250
Erqin-Zeng <112789093+Erqin-Zeng@users.noreply.github.com>
2023-01-29 15:29:54 -0500

    Update elgamal.py

--- elgamal.py
+++ elgamal.py
@@ -3,18 +3,22 @@ import random
 from params import p
 from params import g
 
+
 def keygen():
     sk = random.randint(1, p)
     pk = pow(g, sk, p)
     return pk, sk
 
-def encrypt(pk,m):
-    r = random.randint(1,(p-1)/2)
+
+def encrypt(pk, m):
+    q = (p-1)/2
+    r = random.randint(1, q)
     c1 = pow(g, r, p)
-    c2 = pow(pow(pk, r)*m, 1, p)
+    c1_2 = pow(pk, r, p)
+    c2 = pow(c1_2*m, 1, p)
     return [c1, c2]
 
-def decrypt(sk,c):
+
+def decrypt(sk, c):
     m = pow(c[1]/pow(c[0], sk), 1, p)
     return m
-
