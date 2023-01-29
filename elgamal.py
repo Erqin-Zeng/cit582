328248b798dd954f16e8042d95402753154a1a2c
Erqin-Zeng <112789093+Erqin-Zeng@users.noreply.github.com>
2023-01-29 14:55:29 -0500

    Add files via upload

---
+++ elgamal.py
@@ -0,0 +1,20 @@
+import random
+
+from params import p
+from params import g
+
+def keygen():
+    sk = random.randint(1, p)
+    pk = pow(g, sk, p)
+    return pk, sk
+
+def encrypt(pk,m):
+    r = random.randint(1,(p-1)/2)
+    c1 = pow(g, r, p)
+    c2 = pow(pow(pk, r)*m, 1, p)
+    return [c1, c2]
+
+def decrypt(sk,c):
+    m = pow(c[1]/(pow(c[0],sk)),1,p)
+    return m
+
