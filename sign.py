1685056a942513214db43de7df17693e49728005
Erqin-Zeng <112789093+Erqin-Zeng@users.noreply.github.com>
2023-01-30 22:33:41 -0500

    Update sign.py
    add G to gen_keypair()

--- sign.py
+++ sign.py
@@ -7,9 +7,9 @@ from hashlib import sha256
 def sign(m):
 	#generate public key
 	#Your code here
-	#G = secp256k1.G
+	G = secp256k1.G
 	#n = G.order()
-	private_key, public_key = fastecdsa.gen_keypair()
+	private_key, public_key = fastecdsa.gen_keypair(G)
 
 
 	public_key = public_key
@@ -24,3 +24,8 @@ def sign(m):
 
 	#r = pow(x1, 1, n)
 	#s = 0
+
+	assert isinstance( public_key, point.Point )
+	assert isinstance( r, int )
+	assert isinstance( s, int )
+	return( public_key, [r,s] )
