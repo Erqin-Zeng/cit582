aee8550bdd5023d9d8851007304dacf113276523
Erqin-Zeng <112789093+Erqin-Zeng@users.noreply.github.com>
2023-01-30 23:42:56 -0500

    Update sign.py

--- sign.py
+++ sign.py
@@ -8,14 +8,14 @@ from hashlib import sha256
 def sign(m):
 	# generate public key
 	# Your code here
-	# private_key, public_key = gen_keypair(curve.secp256k1)
-	private_key = keys.gen_private_key(curve.secp256k1)
-	public_key = keys.get_public_key(private_key, curve.secp256k1)
+	private_key, public_key = gen_keypair(curve.secp256k1)
+	# private_key = keys.gen_private_key(curve.secp256k1)
+	# public_key = keys.get_public_key(private_key, curve.secp256k1)
 
 	# generate signature
 	# Your code here
 	r, s = ecdsa.sign(m, private_key, hashfunc=sha256)
-	ecdsa.verify((r, s), m, public_key, hashfunc=sha256)
+	# ecdsa.verify((r, s), m, public_key, hashfunc=sha256)
 
 	assert isinstance(public_key, point.Point)
 	assert isinstance(r, int)
