6c31bcb5ed2244099103bb580f41c43696e2af33
Erqin-Zeng <112789093+Erqin-Zeng@users.noreply.github.com>
2023-01-30 23:39:42 -0500

    Update sign.py
    generate keys seperatly

--- sign.py
+++ sign.py
@@ -8,11 +8,15 @@ from hashlib import sha256
 def sign(m):
 	# generate public key
 	# Your code here
-	private_key, public_key = gen_keypair(curve.secp256k1)
+	# private_key, public_key = gen_keypair(curve.secp256k1)
+	private_key = keys.gen_private_key(curve.secp256k1)
+	public_key = keys.get_public_key(private_key, curve.secp256k1)
 
 	# generate signature
 	# Your code here
 	r, s = ecdsa.sign(m, private_key, hashfunc=sha256)
+	valid = ecdsa.verify((r, s), m, public_key, hashfunc=sha256)
+
 	assert isinstance(public_key, point.Point)
 	assert isinstance(r, int)
 	assert isinstance(s, int)
