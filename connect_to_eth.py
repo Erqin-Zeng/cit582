ab894eda2177d5206faa09eb0ac7dcde7db75bac
Erqin-Zeng <112789093+Erqin-Zeng@users.noreply.github.com>
2023-02-05 21:05:13 -0500

    Update connect_to_eth.py
    liane15-17 POA issue

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -1,5 +1,6 @@
 from web3 import Web3
 from web3.providers.rpc import HTTPProvider
+from web3.auto.gethdev import w3
 
 
 # If you use one of the suggested infrastructure providers, the url will be of the form
@@ -11,6 +12,9 @@ def connect_to_eth():
     url = f"https://celo-mainnet.infura.io/v3/4a845c8000094d8f91e8bb909b20773a"
     w3 = Web3(HTTPProvider(url))
     assert w3.isConnected(), f"Failed to connect to provider at {url}"
+    # confirm that the connection succeeded
+    w3.clientVersion
+    'Geth/v1.7.3-stable-4bb3c89d/linux-amd64/go1.9'
     return w3
 
 
