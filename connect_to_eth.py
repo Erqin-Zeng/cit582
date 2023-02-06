2c5cd9f969eedbc8fe8da3f989e5c3af7284b80b
Erqin-Zeng <112789093+Erqin-Zeng@users.noreply.github.com>
2023-02-05 21:09:05 -0500

    Update connect_to_eth.py
    line 19-20 middle onion

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -1,6 +1,8 @@
 from web3 import Web3
 from web3.providers.rpc import HTTPProvider
 from web3.auto.gethdev import w3
+from web3.middleware import geth_poa_middleware
+
 
 
 # If you use one of the suggested infrastructure providers, the url will be of the form
@@ -12,6 +14,11 @@ def connect_to_eth():
     url = f"https://celo-mainnet.infura.io/v3/4a845c8000094d8f91e8bb909b20773a"
     w3 = Web3(HTTPProvider(url))
     assert w3.isConnected(), f"Failed to connect to provider at {url}"
+
+    
+    # inject the poa compatibility middleware to the innermost layer (0th layer)
+    w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
     # confirm that the connection succeeded
     w3.clientVersion
     'Geth/v1.7.3-stable-4bb3c89d/linux-amd64/go1.9'
