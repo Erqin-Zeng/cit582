0bcdb735289ff8cfb1ea9befef001c8aa4ade2a5
Erqin-Zeng <112789093+Erqin-Zeng@users.noreply.github.com>
2023-02-11 00:23:19 -0500

    Update ipfs.py
    updated function 1

--- ipfs.py
+++ ipfs.py
@@ -3,17 +3,21 @@ import json
 
 
 def pin_to_ipfs(data):
-	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
+    assert isinstance(data, dict), f"Error pin_to_ipfs expects a dictionary"
     # code here:
+    json_data = json.dumps(data)   
+    response = requests.post('https://ipfs.infura.io:5001/api/v0/add', json_data)
+    p = response.json()
+    cid = p['Hash']
     return cid
 
 
-def get_from_ipfs(cid,content_type="json"):
-    assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
-    #code here:
+def get_from_ipfs(cid, content_type="json"):
+    assert isinstance(cid, str), f"get_from_ipfs accepts a cid in the form of a string"
+    # code here:
     url_part = 'https://ipfs.infura.io:5001/api/v0/cat?arg='
     url = url_part + cid
     json_data = requests.get(url)
-    data = json.loads(json_data)
-	assert isinstance(data,dict), f"get_from_ipfs should return a dict"
-	return data
+    data = json.loads(json_data.text)
+    assert isinstance(data, dict), f"get_from_ipfs should return a dict"
+    return data
