bea601333e0ea14987993dc472a4c17c0571e851
Erqin-Zeng <112789093+Erqin-Zeng@users.noreply.github.com>
2023-02-11 01:52:55 -0500

    Update sign.py
    connected to infura

--- sign.py
+++ sign.py
@@ -1,23 +1,25 @@
-import fastecdsa
-from fastecdsa.curve import secp256k1
-from fastecdsa.keys import export_key, gen_keypair
-from fastecdsa import curve, ecdsa, keys, point
-from hashlib import sha256
+import requests
+import json
+
+def pin_to_ipfs(data):
+    assert isinstance(data, dict), f"Error pin_to_ipfs expects a dictionary"
+    # code here:
+    infura_url = "https://ipfs.infura.io/ipfs/"
+    project_id = "2La77rgcqFxHLaq1C2Q9JggeJzF"
+    url = f'{infura_url}{project_id}'
+    json_data = json.dumps(data)
+    response = requests.post(url, headers={'Content-Type': 'application/json'}, data=json_data.encode("utf-8"))
+    p = response.json()
+    cid = p['Hash']
+    return cid
 
 
-def sign(m):
-	# generate public key
-	# Your code here
-	private_key, public_key = gen_keypair(curve.secp256k1)
-	# private_key = keys.gen_private_key(curve.secp256k1)
-	# public_key = keys.get_public_key(private_key, curve.secp256k1)
-
-	# generate signature
-	# Your code here
-	r, s = ecdsa.sign(m, private_key, curve.secp256k1, hashfunc=sha256)
-	# ecdsa.verify((r, s), m, public_key, hashfunc=sha256)
-
-	assert isinstance(public_key, point.Point)
-	assert isinstance(r, int)
-	assert isinstance(s, int)
-	return public_key, [r, s]
+def get_from_ipfs(cid, content_type="json"):
+    assert isinstance(cid, str), f"get_from_ipfs accepts a cid in the form of a string"
+    # code here:
+    url_part = 'https://ipfs.infura.io:5001/api/v0/cat?arg='
+    url = url_part + cid
+    json_data = requests.get(url)
+    data = json.loads(json_data.text)
+    assert isinstance(data, dict), f"get_from_ipfs should return a dict"
+    return data
