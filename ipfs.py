ec91b6e6ca191821e6b9134737cf72279be3fadf
Erqin-Zeng <112789093+Erqin-Zeng@users.noreply.github.com>
2023-02-11 00:43:19 -0500

    Update ipfs.py
    data=data

--- ipfs.py
+++ ipfs.py
@@ -6,7 +6,7 @@ def pin_to_ipfs(data):
     assert isinstance(data, dict), f"Error pin_to_ipfs expects a dictionary"
     # code here:
     # json_data = json.dumps(data)
-    response = requests.post('https://ipfs.infura.io:5001/api/v0/add', data)
+    response = requests.post('https://ipfs.infura.io:5001/api/v0/add', data=data)
     p = response.json()
     cid = p['Hash']
     return cid
