b1a9928c265ac99feb96584647b5ff26b8412a80
Erqin-Zeng <112789093+Erqin-Zeng@users.noreply.github.com>
2023-02-11 02:11:24 -0500

    Update ipfs.py
    updated get function

--- ipfs.py
+++ ipfs.py
@@ -9,7 +9,7 @@ def pin_to_ipfs(data):
     project_id = "2La77rgcqFxHLaq1C2Q9JggeJzF"
     url = f'{infura_url}{project_id}'
     response = requests.post(url, headers={'Content-Type': 'application/json'}, data=json_data.encode("utf-8"))
-    p = response.json()
+    p = json.loads(response.text)
     cid = p['Hash']
     return cid
 
@@ -17,9 +17,8 @@ def pin_to_ipfs(data):
 def get_from_ipfs(cid, content_type="json"):
     assert isinstance(cid, str), f"get_from_ipfs accepts a cid in the form of a string"
     # code here:
-    infura_url = "https://ipfs.infura.io/ipfs/"
-    project_id = "2La77rgcqFxHLaq1C2Q9JggeJzF"
-    url = f'{infura_url}{project_id}'
+    infura_url = "https://ipfs.infura.io:5001/api/v0/cat?arg="
+    url = f'{infura_url}{cid}'
     json_data = requests.get(url)
     data = json.loads(json_data.text)
     assert isinstance(data, dict), f"get_from_ipfs should return a dict"
