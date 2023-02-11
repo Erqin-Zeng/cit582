9cf1e827add0d8c26ab7ee13ec516d4d7eb945b2
Erqin-Zeng <112789093+Erqin-Zeng@users.noreply.github.com>
2023-02-11 02:26:49 -0500

    Update ipfs.py
    added secret key

--- ipfs.py
+++ ipfs.py
@@ -1,14 +1,17 @@
 import requests
 import json
 
+
 def pin_to_ipfs(data):
     assert isinstance(data, dict), f"Error pin_to_ipfs expects a dictionary"
     # code here:
     json_data = json.dumps(data)
-    infura_url = "https://ipfs.infura.io/ipfs/"
+    # infura_url = "https://ipfs.infura.io/ipfs/"
+    # url = f'{infura_url}{project_id}'
+    infura_url = 'https://ipfs.infura.io:5001/api/v0/add'
     project_id = "2La77rgcqFxHLaq1C2Q9JggeJzF"
-    url = f'{infura_url}{project_id}'
-    response = requests.post(url, headers={'Content-Type': 'application/json'}, data=json_data.encode("utf-8"))
+    project_secret = 'fc4db061014f8f7db76ae4ca4c152ac3'  
+    response = requests.post(infura_url, headers={'Content-Type': 'application/json'}, data=json_data.encode("utf-8"), auth=(project_id,project_secret))
     p = response.json()
     cid = p['Hash']
     return cid
