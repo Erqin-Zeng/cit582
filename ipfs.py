f97deff00cc0aade384a6f8fd680a15cc5f3e7db
Erqin-Zeng <112789093+Erqin-Zeng@users.noreply.github.com>
2023-02-11 11:35:11 -0500

    Update ipfs.py
    changed to file

--- ipfs.py
+++ ipfs.py
@@ -6,12 +6,13 @@ def pin_to_ipfs(data):
     assert isinstance(data, dict), f"Error pin_to_ipfs expects a dictionary"
     # code here:
     json_data = json.dumps(data)
+    files = {'file':json_data}
     # infura_url = "https://ipfs.infura.io/ipfs/"
     # url = f'{infura_url}{project_id}'
     infura_url = 'https://ipfs.infura.io:5001/api/v0/add'
     project_id = "2La77rgcqFxHLaq1C2Q9JggeJzF"
-    project_secret = 'fc4db061014f8f7db76ae4ca4c152ac3'  
-    response = requests.post(infura_url, headers={'Content-Type': 'application/json'}, data=json_data.encode("utf-8"), auth=(project_id,project_secret))
+    project_secret = 'fc4db061014f8f7db76ae4ca4c152ac3'
+    response = requests.post(infura_url, files=files, auth=(project_id, project_secret))
     p = response.json()
     cid = p['Hash']
     return cid
