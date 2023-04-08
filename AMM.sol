96841262115464608cc60583f867af1c1eee5793
Erqin-Zeng <112789093+Erqin-Zeng@users.noreply.github.com>
2023-04-08 00:32:29 -0400

    Update AMM.sol
    按没有fee算，算出B按有fee比例减去一部分

--- AMM.sol
+++ AMM.sol
@@ -58,21 +58,21 @@ contract AMM is AccessControl{
 		//YOUR CODE HERE 
     
     if (sellToken == tokenA) {    
-      qtyA = 0;
+      //qtyA = 0;
       qtyA = ((10000-feebps)/10000)*sellAmount;
-      qtyB = ERC20(tokenB).balanceOf(address(this)) - invariant/(ERC20(tokenA).balanceOf(address(this))+qtyA);
-      swapAmt = qtyB;
-      ERC20(tokenA).transferFrom(msg.sender, address(this), qtyA);
+      ERC20(tokenA).transferFrom(msg.sender, address(this), sellAmount);
+      qtyB = ERC20(tokenB).balanceOf(address(this)) - invariant/ERC20(tokenA).balanceOf(address(this));
+      swapAmt = qtyA*qtyB/sellAmount;    
       ERC20(tokenB).transfer(msg.sender, swapAmt);
       emit Swap( tokenA, tokenB, sellAmount, swapAmt);
     } 
 
     if (sellToken == tokenB) {  
-      qtyB = 0;
+      //qtyB = 0;
       qtyB = ((10000-feebps)/10000)*sellAmount;
-      qtyA = ERC20(tokenA).balanceOf(address(this)) - invariant/(ERC20(tokenB).balanceOf(address(this))+qtyB);
-      swapAmt = qtyA;
-      ERC20(tokenB).transferFrom(msg.sender, address(this), qtyB);
+      ERC20(tokenB).transferFrom(msg.sender, address(this), sellAmount);
+			qtyA = ERC20(tokenA).balanceOf(address(this)) - invariant/ERC20(tokenB).balanceOf(address(this));
+      swapAmt = qtyB*qtyA/sellAmount;
       ERC20(tokenA).transfer(msg.sender, swapAmt);
       emit Swap( tokenB, tokenA, sellAmount, swapAmt);
     }
