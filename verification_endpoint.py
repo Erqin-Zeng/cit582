ae47ad5ca89491cbf7e6fa29e167128dbb592f33
Erqin-Zeng <112789093+Erqin-Zeng@users.noreply.github.com>
2023-04-16 13:20:41 -0400

    Update verification_endpoint.py
    testing on print

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -11,6 +11,7 @@ app.url_map.strict_slashes = False
 @app.route('/verify', methods=['GET','POST'])
 def verify():
     content = request.get_json(silent=True)
+    print (content["payload"]["platform"])
     print (content)
     #Check if signature is valid
 
