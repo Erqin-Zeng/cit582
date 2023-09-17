15a08e7d1ae374f97ebad550512533833b3b5e43
Erqin-Zeng <112789093+Erqin-Zeng@users.noreply.github.com>
2023-09-17 02:24:09 -0400

    Update ZK_equality.py

--- ZK_equality.py
+++ ZK_equality.py
@@ -1,24 +1,34 @@
-from zksk import Secret, DLRep, utils
+from zksk import Secret, DLRep
+from zksk import utils
 
 def ZK_equality(G, H):
-    # Generate two random secrets for r1 and r2
+    # Generate two random secrets r1 and r2
     r1 = Secret(utils.get_random_num(bits=128))
     r2 = Secret(utils.get_random_num(bits=128))
 
-    # Generate a random secret for the plaintext m
-    m = Secret(utils.get_random_num(bits=128))
-
-    # Create El-Gamal ciphertexts (C1, C2) and (D1, D2)
+    # Create two elliptic curve points C1 and C2
     C1 = r1 * G
-    C2 = r1 * H + m * G
+    C2 = r1 * H
+
+    # Generate a random bit m (0 or 1)
+    m = Secret(utils.get_random_num(bits=1))
+
+    # Update C2 with the addition of m*G
+    C2 += m.value * G
+
+    # Create two elliptic curve points D1 and D2
     D1 = r2 * G
-    D2 = r2 * H + m * G
+    D2 = r2 * H
 
-    # Define the proof statement for equality of plaintexts
-    stmt = DLRep(C1, r1 * G) & DLRep(C2, r1 * H + m * G) & DLRep(D1, r2 * G) & DLRep(D2, r2 * H + m * G)
+    # Update D2 with the addition of m*G
+    D2 += m.value * G
 
-    # Generate a non-interactive zero-knowledge proof
+    # Define the proof statement for both scenarios (m=0 and m=1)
+    stmt = (DLRep(C1, r1 * G) & DLRep(C2, r1 * H + m * G)) | (DLRep(D1, r2 * G) & DLRep(D2, r2 * H + m * G))
+
+    # Generate a non-interactive zero-knowledge proof for the statement
     zk_proof = stmt.prove()
 
-    # Return the two ciphertexts and the proof
-    return (C1, C2), (D1, D2), zk_proof
+    # Return the elliptic curve points C1, C2, D1, D2, and the proof
+    return C1, C2, D1, D2, zk_proof
+
