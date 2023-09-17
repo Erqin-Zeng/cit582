a99af4d79c095ba67fbfb7f49f623a32cf5736fd
Erqin-Zeng <112789093+Erqin-Zeng@users.noreply.github.com>
2023-09-17 04:41:45 -0400

    Update get_ape_info.py

--- get_ape_info.py
+++ get_ape_info.py
@@ -3,6 +3,69 @@ from web3.contract import Contract
 from web3.providers.rpc import HTTPProvider
 import requests
 import json
+
+bayc_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
+contract_address = Web3.to_checksum_address(bayc_address)
+
+# You will need the ABI to connect to the contract
+# The file 'abi.json' has the ABI for the bored ape contract
+# In general, you can get contract ABIs from etherscan
+# https://api.etherscan.io/api?module=contract&action=getabi&address=0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D
+with open('/home/codio/workspace/abi.json', 'r') as f:
+    abi = json.load(f)
+
+# Connect to an Ethereum node
+api_url = # YOU WILL NEED TO PROVIDE THE URL OF AN ETHEREUM NODE
+provider = HTTPProvider(api_url)
+web3 = Web3(provider)
+
+def get_ape_info(apeID):
+    assert isinstance(apeID, int), f"{apeID} is not an int"
+    assert 1 <= apeID, f"{apeID} must be at least 1"
+
+    data = {'owner': "", 'image': "", 'eyes': "" }
+
+    try:
+        # Get the owner's address
+        owner = contract.functions.ownerOf(apeID).call()
+        data['owner'] = owner
+
+        # Get the tokenURI
+        tokenURI = contract.functions.tokenURI(apeID).call()
+
+        # Replace 'ipfs://' with the IPFS gateway URL
+        tokenURI = tokenURI.replace('ipfs://', 'https://gateway.pinata.cloud/ipfs/')
+
+        data['image'] = tokenURI
+
+        # Fetch additional data from IPFS if needed
+        response = requests.get(tokenURI)
+        if response.status_code == 200:
+            ipfs_data = response.json()
+            if 'attributes' in ipfs_data:
+                attributes = ipfs_data['attributes']
+                for attribute in attributes:
+                    if 'trait_type' in attribute and attribute['trait_type'] == 'Eyes':
+                        data['eyes'] = attribute['value']
+                        break
+
+    except Exception as e:
+        print(f"Error fetching data for Ape {apeID}: {str(e)}")
+
+    assert isinstance(data, dict), f'get_ape_info({apeID}) should return a dict'
+    assert all([a in data.keys() for a in ['owner', 'image', 'eyes']]), f"Return value should include the keys 'owner', 'image', and 'eyes'"
+
+    return data
+
+
+
+
+'''
+from web3 import Web3
+from web3.contract import Contract
+from web3.providers.rpc import HTTPProvider
+import requests
+import json
 import time
 
 bayc_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
@@ -72,3 +135,4 @@ def get_ape_info(apeID):
     return data
   
 #get_ape_info(1)
+'''
