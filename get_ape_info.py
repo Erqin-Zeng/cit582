74eaaa78aa8380ee3a5ddccc2f639fd1dd25c78d
Erqin-Zeng <112789093+Erqin-Zeng@users.noreply.github.com>
2023-09-17 04:59:54 -0400

    Update get_ape_info.py

--- get_ape_info.py
+++ get_ape_info.py
@@ -8,17 +8,18 @@ import time
 bayc_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
 contract_address = Web3.toChecksumAddress(bayc_address)
 
-# You will need the ABI to connect to the contract
-# The file 'abi.json' has the ABI for the bored ape contract
-# In general, you can get contract ABIs from etherscan
-# https://api.etherscan.io/api?module=contract&action=getabi&address=0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D
+# Load the ABI
 with open('/home/codio/workspace/abi.json', 'r') as f:
-    abi = json.load(f)
+  abi = json.load(f)
 
 # Connect to an Ethereum node
 api_url = "https://mainnet.infura.io/v3/4a845c8000094d8f91e8bb909b20773a"
 provider = HTTPProvider(api_url)
 web3 = Web3(provider)
+
+# Create a contract instance
+contract = web3.eth.contract(address=contract_address, abi=abi)
+
 
 def get_ape_info(apeID):
     assert isinstance(apeID, int), f"{apeID} is not an int"
