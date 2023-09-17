5c94f921dcb3c03e1790b7a58c6bf50a9ad3f419
Erqin-Zeng <112789093+Erqin-Zeng@users.noreply.github.com>
2023-09-17 02:39:04 -0400

    Update ZK_equality.py

--- ZK_equality.py
+++ ZK_equality.py
@@ -5,29 +5,28 @@ def ZK_equality(G, H):
     # Generate two random secrets r1 and r2
     r1 = Secret(utils.get_random_num(bits=128))
     r2 = Secret(utils.get_random_num(bits=128))
-
-    # Create two elliptic curve points C1 and C2
-    C1 = r1 * G
-    C2 = r1 * H
-
+    
     # Generate a random bit m (0 or 1)
     m = Secret(utils.get_random_num(bits=1))
 
-    # Update C2 with the addition of m*G
-    C2 = C2 + (m * G)
+    # Create two elliptic curve points C1 and C2
+    C1 = r1 * G
+    C2 = r1 * H + (m * G)
 
     # Create two elliptic curve points D1 and D2
     D1 = r2 * G
-    D2 = r2 * H
-
-    # Update D2 with the addition of m*G
-    D2 = D2 + (m * G)
+    D2 = r2 * H + (m * G)
 
     # Define the proof statement for both scenarios (m=0 and m=1)
-    stmt = (DLRep(C1, r1 * G) & DLRep(C2, r1 * H + m * G)) | (DLRep(D1, r2 * G) & DLRep(D2, r2 * H + m * G))
+    if m==1:
+        stmt1 = DLRep(C1, r1 * H, simulated=True) | DLRep(C1 - G, r1 * H)
+        stmt2 = DLRep(D1, r2 * H, simulated=True) | DLRep(D1 - G, r2 * H)
+    else: # m=0
+        stmt1 = DLRep(C1, r1 * H) | DLRep(C1 - G, r1 * H,simulated=True)
+        stmt2 = DLRep(D1, r2 * H) | DLRep(D1 - G, r2 * H,simulated=True)
 
     # Generate a non-interactive zero-knowledge proof for the statement
-    zk_proof = stmt.prove()
+    zk_proof = stmt1.prove() & stmt2.prove()
 
     # Return the elliptic curve points C1, C2, D1, D2, and the proof
     return C1, C2, D1, D2, zk_proof
